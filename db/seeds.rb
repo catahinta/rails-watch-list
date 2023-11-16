fe668d663b3e47a75e65f45ef5268c60b375bcb1
Kata Nagy <nagykata98@gmail.com>
2023-11-16 11:45:31 +0000

    Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Movie.destroy_all
+puts "Destroying movies"
+
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialised = URI.open(url).read
+movies = JSON.parse(movies_serialised)
+
+puts "Scraping api"
+
+movies["results"].each do |movie|
+  title = movie["title"]
+  overview = movie["overview"]
+  rating = movie["vote_average"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+  Movie.create(
+    title: title,
+    overview: overview,
+    rating: rating,
+    poster_url: poster_url
+  )
+  puts "Movie created!"
+end
