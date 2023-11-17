374d71715f9f56ba3563f7494b27a769f7640cc5
Kata Nagy <nagykata98@gmail.com>
2023-11-17 16:02:15 +0000

    list background

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,4 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
-  validates :photo, presence: true
 end
